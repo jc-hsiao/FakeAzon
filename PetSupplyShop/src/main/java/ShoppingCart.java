1b6f3661c22b8c09e2feaee0232fe7d07656dd23
jchuru <jameschuru123@gmail.com>
2020-03-13 12:29:02 -0400

    Stubbed ShoppingCart

--- PetSupplyShop/src/main/java/ShoppingCart.java
+++ PetSupplyShop/src/main/java/ShoppingCart.java
@@ -1,2 +1,17 @@
 public class ShoppingCart {
+    // Map of items <Item , Integer quantity >
+
+    // Total of all items - Double
+
+    // boolean  add - Item to cart  (Item item, int amount)
+            // check if itm is > 99 - return false;
+
+    // boolean remove (Item)
+
+    // boolean replaceItemAmount(Item key , int newAmount)
+
+    // boolean increaseItemAmount(Item key , int amountToIncrease)
+
+    
+
 }
