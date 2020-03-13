d7921db767b9dee7c6c6a1f3fe0c05183dc55574
jchuru <jameschuru123@gmail.com>
2020-03-13 17:07:34 -0400

    ShoppingCart Done so Far

--- PetSupplyShop/src/main/java/ShoppingCart.java
+++ PetSupplyShop/src/main/java/ShoppingCart.java
@@ -1,17 +1,80 @@
+import java.util.HashMap;
+
 public class ShoppingCart {
-    // Map of items <Item , Integer quantity >
 
-    // Total of all items - Double
 
-    // boolean  add - Item to cart  (Item item, int amount)
-            // check if itm is > 99 - return false;
+    HashMap<Item,Integer> items;
+    Double cartValue;
 
-    // boolean remove (Item)
 
-    // boolean replaceItemAmount(Item key , int newAmount)
+    public ShoppingCart(){
+        items = new HashMap<Item, Integer>();
+        cartValue = 0.00;
+    }
 
-    // boolean increaseItemAmount(Item key , int amountToIncrease)
+    public boolean add(Item item, int amount){
 
-    
+        if(items.containsKey(item)){
+            items.put(item , items.get(item) + amount);
+            return true;
+        }else if(!items.containsKey(item) && amount > 0){
+            items.put(item , amount);
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    public boolean remove(Item item){
+
+        if(items.containsKey(item)){
+            items.remove(item);
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean replaceItemAmount(Item item , int newAmount){
+
+        if (items.containsKey(item)) {
+            items.put(item, newAmount);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public Integer getItemAmount(Item item){
+        return items.get(item);
+    }
+
+    public boolean increaseItemAmount(Item item , int amountToIncrease){
+
+        if(items.containsKey(item)){
+            items.put(item , items.get(item) + amountToIncrease);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public Integer getNumOfItems(){
+        return this.items.size();
+    }
+
+    public Double getCartValue(){
+        return this.cartValue;
+    }
+
+    public HashMap<Item , Integer> getItems(){
+        return this.items;
+    }
+
+
+
+
+
+
 
 }
