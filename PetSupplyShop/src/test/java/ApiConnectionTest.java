6bec58f9cdf52615d51ed6a204de5a2abc66cddb
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-03-15 14:04:15 -0400

    added query to retrieve json from amazon price api

---
+++ PetSupplyShop/src/test/java/ApiConnectionTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import shop.ApiConnection;
+import static org.junit.Assert.*;
+
+public class ApiConnectionTest {
+
+    @Test
+    public void testCreateApiQuery(){
+        String search = "headphones";
+        String actual =  ApiConnection.createApiQuery(search);
+        String expected = "https://amazon-price1.p.rapidapi.com/search?keywords=headphones&marketplace=US";
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testFetchApiQuery(){
+        String search = "headphones";
+        String resultOfCall = ApiConnection.fetchApiQuery(ApiConnection.createApiQuery(search));
+        System.out.println(resultOfCall);
+    }
+
+}
