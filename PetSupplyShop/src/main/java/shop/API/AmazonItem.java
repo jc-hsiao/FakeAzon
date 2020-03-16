c469671b486eb58338c1eaf6a79c42ff54f5e8ea
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-03-15 23:17:24 -0400

    amazonItem class complete

--- PetSupplyShop/src/main/java/shop/API/AmazonItem.java
+++ PetSupplyShop/src/main/java/shop/API/AmazonItem.java
@@ -12,5 +12,78 @@ public class AmazonItem {
     private int totalReviews;
     private boolean isPrimeEligible;
 
+    public AmazonItem() {}
+
+    public String getASIN() {
+        return ASIN;
+    }
+
+    public void setASIN(String ASIN) {
+        this.ASIN = ASIN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getListPrice() {
+        return listPrice;
+    }
+
+    public void setListPrice(double listPrice) {
+        this.listPrice = listPrice;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDetailPageUrl() {
+        return detailPageUrl;
+    }
+
+    public void setDetailPageUrl(String detailPageUrl) {
+        this.detailPageUrl = detailPageUrl;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getTotalReviews() {
+        return totalReviews;
+    }
+
+    public void setTotalReviews(int totalReviews) {
+        this.totalReviews = totalReviews;
+    }
+
+    public boolean isPrimeEligible() {
+        return isPrimeEligible;
+    }
+
+    public void setPrimeEligible(boolean primeEligible) {
+        isPrimeEligible = primeEligible;
+    }
 
 }
