6c9dcac5da850c340ef1e984a02f0096a06fc298
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-03-15 22:43:54 -0400

    started amazonItem class

--- PetSupplyShop/src/main/java/shop/API/AmazonItem.java
+++ PetSupplyShop/src/main/java/shop/API/AmazonItem.java
@@ -1,4 +1,16 @@
 package shop.API;
 
 public class AmazonItem {
+
+    private String ASIN;
+    private String title;
+    private double price;
+    private double listPrice;
+    private String imageUrl;
+    private String detailPageUrl;
+    private double rating;
+    private int totalReviews;
+    private boolean isPrimeEligible;
+
+
 }
