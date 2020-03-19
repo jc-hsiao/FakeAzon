9a2b6ec6288db8116252fa76ca9b6d73ac70a354
Moe Aydin <maydin@zipcodes-MacBook-Pro-23.local>
2020-03-19 17:29:53 -0400

    didnt do anything

--- PetSupplyShop/src/main/java/shop/sqlcontroller/SQLConnection.java
+++ PetSupplyShop/src/main/java/shop/sqlcontroller/SQLConnection.java
@@ -1,25 +1,55 @@
 package shop.sqlcontroller;
 
-import java.sql.Connection;
-import com.mysql.cj.jdbc.Driver;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
+import com.mysql.cj.jdbc.Driver;
 public class SQLConnection {
 
     public static final String databaseURL = "jdbc:mysql://fakeazon.cn8iyodgb9pu.us-east-1.rds.amazonaws.com?serverTimezone=UTC";
     public static final String user = "justZipCode";
     public static final String password = "timeToCode?";
 
+    public static Connection connection;
 
-    public static Connection getConnection(){
+    public static  void makeConnection(){
         try {
             DriverManager.registerDriver(new Driver());
             System.out.println("Great Success");
-            return DriverManager.getConnection(databaseURL, user, password);
+            connection = DriverManager.getConnection(databaseURL, user, password);
         } catch(SQLException e){
             throw new RuntimeException("Can't Connect", e);
         }
     }
+
+    public static void useDB() {
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("USE fakeAzonDB ");
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection(){
+        makeConnection();
+        useDB();
+        return connection;
+
+    }
+
+    /*
+    *****  TESTING PURPOSES  *******
+    public static void main(String[] args) {
+        getConnection();
+        try{
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from test");
+            sout(rs);
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+     */
+
 
 }
