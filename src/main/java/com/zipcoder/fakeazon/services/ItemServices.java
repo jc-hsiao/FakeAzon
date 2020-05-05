e4ca8e74a47829a95e9224539462151de11d8d73
k-romero <imkevinromero@gmail.com>
2020-05-04 20:59:39 -0400

    add basic crud to items to service. Awaiting meeting to merge service

--- src/main/java/com/zipcoder/fakeazon/services/ItemServices.java
+++ src/main/java/com/zipcoder/fakeazon/services/ItemServices.java
@@ -1,4 +1,40 @@
 package com.zipcoder.fakeazon.services;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+import com.zipcoder.fakeazon.models.Item;
+import com.zipcoder.fakeazon.repositories.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@Service
 public class ItemServices {
+
+    private final ItemRepository repo;
+
+    @Autowired
+    public ItemServices(ItemRepository repo){
+        this.repo = repo;
+    }
+
+    public Optional<Item> findOne(Integer itemId){
+        return repo.findById(itemId);
+    }
+
+    public List<Item> findAll(){
+        return repo.findAll();
+    }
+
+    public Item saveItem(Item item){
+        return repo.save(item);
+    }
+
+    public boolean deleteItem(Integer itemId){
+        if(findOne(itemId).isPresent()){
+            repo.deleteById(itemId);
+            return true;
+        } else
+            return false;
+    }
 }
