eccdd48ec73faa333324ed422999db600b9df58f
muhammeta7 <muhammeta7@gmail.com>
2020-05-06 21:18:42 -0400

    added methods to order service

--- src/main/java/com/zipcoder/fakeazon/repositories/OrderRepository.java
+++ src/main/java/com/zipcoder/fakeazon/repositories/OrderRepository.java
@@ -3,6 +3,12 @@ import com.zipcoder.fakeazon.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Integer> {
+
+    Optional<List<Order>> findOrdersByUser(int id);
+
 }
---
+++ src/test/java/com/zipcoder/fakeazon/services/OrderServiceTest.java
@@ -0,0 +1,5 @@
+package com.zipcoder.fakeazon.services;
+
+public class OrderServiceTest {
+
+}
