941e3ad9afc96b1a7316f77380f7561084b82c83
jc-hsiao <marshfire@live.com>
2020-05-09 00:47:14 -0400

    finish user put method

--- src/main/java/com/zipcoder/fakeazon/services/UserServices.java
+++ src/main/java/com/zipcoder/fakeazon/services/UserServices.java
@@ -1,5 +1,9 @@
 package com.zipcoder.fakeazon.services;
 
+import com.zipcoder.fakeazon.exception.NotFoundException;
+import com.zipcoder.fakeazon.models.Address;
+import com.zipcoder.fakeazon.models.Item;
+import com.zipcoder.fakeazon.models.Shop;
 import com.zipcoder.fakeazon.models.User;
 import com.zipcoder.fakeazon.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,4 +41,44 @@ public class UserServices {
     public List<User> findAllUsers(){
         return userRepo.findAll();
     }
+
+    //put methods
+    public User changeFirstName(Integer id, String newFirstName){
+        User u = checkIfUserExists(id);
+        u.setFirstName(newFirstName);
+        return userRepo.save(u);
+    }
+
+    public User changeLastName(Integer id, String newLastName){
+        User u = checkIfUserExists(id);
+        u.setLastName(newLastName);
+        return userRepo.save(u);
+    }
+
+    public User changeEmail(Integer id, String newEmail){
+        User u = checkIfUserExists(id);
+        u.setEmail(newEmail);
+        return userRepo.save(u);
+    }
+
+    public User changeAddress(Integer id, Address newAddress){
+        User u = checkIfUserExists(id);
+        u.setAddress(newAddress);
+        return userRepo.save(u);
+    }
+
+    public User changeShop(Integer id, Shop newShop){
+        User u = checkIfUserExists(id);
+        u.setShop(newShop);
+        return userRepo.save(u);
+    }
+
+    public User checkIfUserExists(Integer id){
+        Optional<User> user = findUserById(id);
+        if (user.isPresent())
+            return user.get();
+        else throw new NotFoundException("No data found for this user.");
+    }
+
+
 }
